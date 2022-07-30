bbba8a570b2616dafe19cf704925d4f75d9a86cb
Joshua Pawlik <joshuapawlik12@gmail.com>
2022-07-30 14:40:08 -0500

    Use API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import {React, useState} from 'react';
+import useBookSearch from "./useBookSearch";
+
 function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState('')
+
+  function handleSearch(e){
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  useBookSearch(query, pageNumber);
   return (
     <>
+      <input type = "text" onChange={handleSearch}></input>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Loading...</div>
+      <div>Error</div>
+
     </>
   );
 }
