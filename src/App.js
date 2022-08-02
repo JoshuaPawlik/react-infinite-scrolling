e01b1b96235c146f6f7c79bad45380111cf711c0
Joshua Pawlik <joshuapawlik12@gmail.com>
2022-08-01 21:09:09 -0500

    Fix loading bug

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     if (loading) return;
     if (observer.current) observer.current.disconnect();
     observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting){
+      if (entries[0].isIntersecting && hasMore){
         setPageNumber(prevPageNumber => prevPageNumber + 1)
       }
     })
